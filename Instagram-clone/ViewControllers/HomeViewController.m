1d03b8e12d722b3e44cc39512f39c76c79d25ba0
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-07-06 18:34:32 -0400

    Implemented persistent login

--- Instagram-clone/ViewControllers/HomeViewController.m
+++ Instagram-clone/ViewControllers/HomeViewController.m
@@ -7,6 +7,7 @@
 
 #import "HomeViewController.h"
 #import "Parse/Parse.h"
+#import "SceneDelegate.h"
 
 @interface HomeViewController ()
 
@@ -21,7 +22,11 @@
 - (IBAction)didTapLogOut:(id)sender {
     [PFUser logOutInBackgroundWithBlock:^(NSError * _Nullable error) {
         // PFUser.current() will now be nil
-        [self dismissViewControllerAnimated:YES completion:nil];
+        SceneDelegate *myDelegate = (SceneDelegate *)self.view.window.windowScene.delegate;
+
+            UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+            UIViewController *loginViewController = [storyboard instantiateViewControllerWithIdentifier:@"LoginViewController"];
+        myDelegate.window.rootViewController = loginViewController;
         
     }];
 }
