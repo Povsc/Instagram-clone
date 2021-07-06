acd6e1679330c86f671dc59fc2d4a3e2fbf8b588
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-07-06 18:11:13 -0400

    Added new home screen and implemented log out

---
+++ Instagram-clone/ViewControllers/HomeViewController.h
@@ -0,0 +1,16 @@
+//
+//  HomeViewController.h
+//  Instagram-clone
+//
+//  Created by felipeccm on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface HomeViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
