a010aeb27b931e75a1785c9a8832ed691367ebb7
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-07-06 19:41:27 -0400

    configured compose view

--- Instagram-clone/ViewControllers/ComposeViewController.m
+++ Instagram-clone/ViewControllers/ComposeViewController.m
@@ -7,7 +7,8 @@
 
 #import "ComposeViewController.h"
 
-@interface ComposeViewController ()
+@interface ComposeViewController () <UITextViewDelegate>
+@property (weak, nonatomic) IBOutlet UITextView *captionField;
 
 @end
 
@@ -15,13 +16,34 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    self.captionField.delegate = self;
+    
+    // Make caption frame round
+    self.captionField.layer.cornerRadius = 10;
+    self.captionField.layer.masksToBounds = true;
+    
+    // Add placeholder text
+    self.captionField.text = @"Caption...";
+    self.captionField.textColor = [UIColor  lightGrayColor];
 }
 
 - (IBAction)didTapCancel:(id)sender {
     [self dismissViewControllerAnimated:YES completion:nil];
 }
 
+- (void)textViewDidBeginEditing:(UITextView *)textView {
+    if (textView.textColor == [UIColor lightGrayColor]) {
+        textView.text = nil;
+        textView.textColor = [UIColor blackColor];
+    }
+}
+
+- (void)textViewDidEndEditing:(UITextView *)textView {
+    if ([textView.text isEqual:@""]) {
+        textView.text = @"Caption...";
+        textView.textColor = [UIColor lightGrayColor];
+    }
+}
 /*
 #pragma mark - Navigation
 
