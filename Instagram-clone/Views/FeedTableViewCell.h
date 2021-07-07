5db3e917295686f2440f4a0cad6f44af2a0cb41f
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-07-07 16:08:11 -0400

    Added FeedTableViewCell

---
+++ Instagram-clone/Views/FeedTableViewCell.h
@@ -0,0 +1,21 @@
+//
+//  FeedTableViewCell.h
+//  Instagram-clone
+//
+//  Created by felipeccm on 7/7/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FeedTableViewCell : UITableViewCell
+@property (weak, nonatomic) IBOutlet UILabel *authorLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *postImageView;
+@property (weak, nonatomic) IBOutlet UIButton *likeButton;
+@property (weak, nonatomic) IBOutlet UILabel *likeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *captionLabel;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Instagram-clone/Views/FeedTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  FeedTableViewCell.m
+//  Instagram-clone
+//
+//  Created by felipeccm on 7/7/21.
+//
+
+#import "FeedTableViewCell.h"
+
+@implementation FeedTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
