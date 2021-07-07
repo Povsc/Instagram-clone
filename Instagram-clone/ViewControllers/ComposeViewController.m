672f537e772c29c1ed35d327531428db83e82792
Felipe Conte Cortez Martins <felipeccm@fb.com>
2021-07-07 12:52:54 -0400

    Implemented image resize function

--- Instagram-clone/ViewControllers/ComposeViewController.m
+++ Instagram-clone/ViewControllers/ComposeViewController.m
@@ -77,6 +77,22 @@
     [self.view endEditing:TRUE];
 }
 
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
+}
+
+
+
 /*
 #pragma mark - Navigation
 
